0aef620ac68afc69b1a060263e15bea759b81ad7
Michael Zhang <failed.down@gmail.com>
2018-02-09 03:27:14 -0600

    f

--- source/clear.c
+++ source/clear.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-int main() { execve("/bin/sh", NULL, NULL); }
+int main() {
+    char *home = getenv("HOME");
+    char home_env[6 + strlen(home)];
+    sprintf(home_env, "HOME=%s", home);
+    char *envp[2] = {home_env, NULL};
+    execve("/bin/sh", NULL, envp);
+}
